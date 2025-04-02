74ebe7d14c33c87ca6b90ca60179131e0bcef672
Bernardo Alves <bernardomiguelalves34@gmail.com>
2025-04-02 09:23:00 +0100

    Inicio

---
+++ Sistema_Integrado_de_Gestao_de_Horarios_e_Recursos_SIGHR/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Sistema_Integrado_de_Gestao_de_Horarios_e_Recursos_SIGHR.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
