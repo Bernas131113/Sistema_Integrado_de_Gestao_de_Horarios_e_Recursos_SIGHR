8ab19def4d98f9d134ce0437d2a2ece8435d6908
Bruno Passarinho <Bruno Passarinho@LAPTOP-GE9G1CHB>
2025-04-09 12:05:02 +0100

    Utilizador

--- Sistema_Integrado_de_Gestao_de_Horarios_e_Recursos_SIGHR/Models/Utilizador.cs
+++ Sistema_Integrado_de_Gestao_de_Horarios_e_Recursos_SIGHR/Models/Utilizador.cs
@@ -1,10 +1,26 @@
 ﻿namespace Sistema_Integrado_de_Gestao_de_Horarios_e_Recursos_SIGHR.Models
 {
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+
     public class Utilizador
     {
-        public int Id { get; set; }
-        public string? Nome { get; set; }
-        public string? PIN { get; set; }
-        public string? Tipo { get; set; } // Administrador, Funcionário de Escritório, Colaborador
+        public long Id { get; set; }
+
+        [Required]
+        public string Nome { get; set; }
+
+        [Required]
+        public string Username { get; set; }
+
+        [Required]
+        public int PIN { get; set; }
+
+        [Required]
+        public string Tipo { get; set; }
+
+        public ICollection<Horario> Horarios { get; set; }
+        public ICollection<Falta> Falta { get; set; }
+        public ICollection<Encomenda> Encomendas { get; set; }
     }
 }
